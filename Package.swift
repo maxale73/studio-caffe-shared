bc59dd1860d8d57b523149c14b0e847c1209dca6
Massimo Di Leonardo <massimodileonardo@gmail.com>
2021-05-17 06:37:41 +0200

    7

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "studio-caffe-shared",
     platforms: [
        .macOS(.v10_15),
-        .iOS(.v14)
+        .iOS(SupportedPlatform.IOSVersion.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
