9e825fe61d24fc2d310d3fe670ebac968389d72d
Massimo Di Leonardo <massimodileonardo@gmail.com>
2021-05-21 05:56:12 +0200

    __

---
+++ Sources/studio-caffe-shared/Routing/Endpoints/PurchaseEndpointsGroup.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public struct PurchaseEndpointsGroup: EndpointGroupType {
+    
+    public static var group = "purchase"
+    
+    public static func createDocument(document: Body? = nil) -> EndpointConfiguration {
+        let parameters = [
+            PathParameter(name: "create_document", value: nil),
+        ]
+        let constructor = PathConstructor(group: group, elements: parameters)
+        return EndpointConfiguration(pathConstructor: constructor, method: .post, body: document)
+    }
+    
+    public static func fetchProductsFromDocument(document: Body? = nil) -> EndpointConfiguration {
+        let parameters = [
+            PathParameter(name: "fetch_products_from_document", value: nil),
+        ]
+        let constructor = PathConstructor(group: group, elements: parameters)
+        return EndpointConfiguration(pathConstructor: constructor, method: .post, body: document)
+    }
+    
+    public static func fetchDocumentsWithFilter(filter: Body? = nil) -> EndpointConfiguration {
+        let parameters = [
+            PathParameter(name: "fetch_documents_with_filter", value: nil),
+        ]
+        let constructor = PathConstructor(group: group, elements: parameters)
+        return EndpointConfiguration(pathConstructor: constructor, method: .post, body: filter)
+    }
+}
