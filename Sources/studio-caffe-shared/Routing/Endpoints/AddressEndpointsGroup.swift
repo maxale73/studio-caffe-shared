7a2ffa1c38a4cc9e0e5a7f4b5a608e8f0d0b3449
Massimo Di Leonardo <massimodileonardo@gmail.com>
2021-05-24 07:31:45 +0200

    __

---
+++ Sources/studio-caffe-shared/Routing/Endpoints/AddressEndpointsGroup.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public struct AddressEndpointsGroup: EndpointGroupType {
+    
+    public static var group = "address"
+    
+    public static func save(address: Body? = nil) -> EndpointConfiguration {
+        let parameters = [
+            PathParameter(name: "save", value: nil)
+        ]
+        let constructor = PathConstructor(group: group, elements: parameters)
+        return EndpointConfiguration(pathConstructor: constructor, method: .post, body: address)
+    }
+    
+    public static func delete(addressID: UUID? = nil) -> EndpointConfiguration {
+        let parameters = [
+            PathParameter(name: "delete", value: nil),
+            PathParameter(name: "address_ID", value: .uuid(addressID))
+        ]
+        let constructor = PathConstructor(group: group, elements: parameters)
+        return EndpointConfiguration(pathConstructor: constructor, method: .post)
+    }
+}
