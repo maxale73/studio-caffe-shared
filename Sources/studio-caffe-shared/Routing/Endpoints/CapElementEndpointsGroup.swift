31a31fccd5d51cf2295fbbe316a589752b56467a
Massimo Di Leonardo <massimodileonardo@gmail.com>
2021-05-28 17:30:26 +0200

    __

---
+++ Sources/studio-caffe-shared/Routing/Endpoints/CapElementEndpointsGroup.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public struct CapElementEndpointsGroup: EndpointGroupType {
+    
+    public static var group = "capElement"
+    
+    public static func index() -> EndpointConfiguration {
+        let parameters = [
+            PathParameter(name: "index", value: nil)
+        ]
+        let constructor = PathConstructor(group: group, elements: parameters)
+        return EndpointConfiguration(pathConstructor: constructor, method: .get)
+    }
+}
