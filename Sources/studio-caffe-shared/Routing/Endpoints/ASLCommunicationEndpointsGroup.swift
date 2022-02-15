80d6a5d264098c210aa8213ce16138869fa4319e
Massimo Di Leonardo <massimodileonardo@gmail.com>
2022-02-15 11:17:51 +0100

    ASL Comm Endpoints refinements

--- Sources/studio-caffe-shared/Routing/Endpoints/ASLCommunicationEndpointsGroup.swift
+++ Sources/studio-caffe-shared/Routing/Endpoints/ASLCommunicationEndpointsGroup.swift
@@ -38,14 +38,22 @@ public struct ASLCommunicationsBySP: Identifiable, Equatable, Codable {
     public var id: UUID
     public var communications: [ASLCommunicationDTO]
     public var customer: String
-    public var sellingPoint: String
+    public var sellingPoint: Int
     public var ubicazione: String
+    
+    public init(id: UUID, communications: [ASLCommunicationDTO], customer: String, sellingPoint: Int, ubicazione: String) {
+        self.id = id
+        self.communications = communications
+        self.customer = customer
+        self.sellingPoint = sellingPoint
+        self.ubicazione = ubicazione
+    }
 }
 
 public struct ASLCommunicationEndpointsGroup: EndpointGroupType {
     public static var group = "asl_communication"
     
-    public static func create(communication: RequestBody? = nil) -> EndpointConfiguration {
+    public static func save(communication: RequestBody? = nil) -> EndpointConfiguration {
         let parameters = [
             PathParameter(name: "create", value: nil)
         ]
@@ -53,7 +61,7 @@ public struct ASLCommunicationEndpointsGroup: EndpointGroupType {
         return EndpointConfiguration(pathConstructor: constructor, method: .post, body: communication)
     }
     
-    public static func indexBySellingPoint(sellingPointID: UUID) -> EndpointConfiguration {
+    public static func indexBySellingPoint(sellingPointID: UUID? = nil) -> EndpointConfiguration {
         let parameters = [
             PathParameter(name: "index_by_selling_point", value: nil),
             PathParameter(name: "selling_point_id", value: .uuid(sellingPointID))
