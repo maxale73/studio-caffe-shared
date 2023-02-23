11f6a9c1b5bb97677f5ad7efa5bb65f80a8a9f4d
Massimo Di Leonardo <massimodileonardo@gmail.com>
2023-02-23 07:46:33 +0100

    __

--- Sources/studio-caffe-shared/Model/AssetsDocumentType.swift
+++ Sources/studio-caffe-shared/Model/AssetsDocumentType.swift
@@ -1,8 +1,12 @@
 import Foundation
 
-public enum AssetsDocumentType: String, CaseIterable, RawRepresentable, Codable, Identifiable {
+public enum AssetsDocumentType: String, CaseIterable, RawRepresentable, Codable, Identifiable, CustomStringConvertible {
     
     public var id: String { rawValue }
     case acquisto
     case vendita
+    
+    public var description: String {
+        rawValue
+    }
 }
