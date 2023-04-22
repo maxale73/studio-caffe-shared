b968275821b2796237b82c36e97afcacf4948b77
Massimo Di Leonardo <massimodileonardo@gmail.com>
2023-04-22 05:36:45 +0200

    __

--- Sources/studio-caffe-shared/Routing/Endpoints/CustomerEndpointsGroup.swift
+++ Sources/studio-caffe-shared/Routing/Endpoints/CustomerEndpointsGroup.swift
@@ -1,5 +1,60 @@
 import Foundation
 
+public struct CustomerCompleteDTO: Codable, Identifiable, Hashable, Equatable {
+    
+    public init(id: UUID, businessName: String, alias: String? = nil, partitaIva: String? = nil, codiceFiscale: String? = nil, cuFatturazione: String? = nil, iban: String? = nil, referencePerson: String, notes: [String]? = nil, addresses: [AddressBySP], phoneNumbers: [PhoneNumberDTO], emails: [EmailDTO], ristorni: [RistornoDTO]) {
+        self.id = id
+        self.businessName = businessName
+        self.alias = alias
+        self.partitaIva = partitaIva
+        self.codiceFiscale = codiceFiscale
+        self.cuFatturazione = cuFatturazione
+        self.iban = iban
+        self.referencePerson = referencePerson
+        self.notes = notes
+        self.addresses = addresses
+        self.phoneNumbers = phoneNumbers
+        self.emails = emails
+        self.ristorni = ristorni
+    }
+    
+    
+    public var id: UUID
+    public var businessName: String
+    public var alias: String?
+    public var partitaIva: String?
+    public var codiceFiscale: String?
+    public var cuFatturazione: String?
+    public var iban: String?
+    public var referencePerson: String
+    public var notes: [String]?
+    public var addresses: [AddressBySP]
+    public var phoneNumbers: [PhoneNumberDTO]
+    public var emails: [EmailDTO]
+    public var ristorni: [RistornoDTO]
+    
+    public static func == (lhs: CustomerCompleteDTO, rhs: CustomerCompleteDTO) -> Bool {
+        return lhs.id == rhs.id &&
+            lhs.businessName == rhs.businessName &&
+            lhs.alias == rhs.alias &&
+            lhs.partitaIva == rhs.partitaIva &&
+            lhs.codiceFiscale == rhs.codiceFiscale &&
+            lhs.cuFatturazione == rhs.cuFatturazione &&
+            lhs.iban == rhs.iban &&
+            lhs.referencePerson == rhs.referencePerson &&
+            lhs.notes == rhs.notes &&
+            lhs.addresses == rhs.addresses &&
+            lhs.phoneNumbers == rhs.phoneNumbers &&
+            lhs.emails == rhs.emails &&
+            lhs.ristorni == rhs.ristorni
+    }
+    
+    public static var empty: CustomerCompleteDTO {
+        CustomerCompleteDTO(id: .init(), businessName: "", alias: nil, partitaIva: nil, codiceFiscale: nil, cuFatturazione: nil, iban: nil, referencePerson: "", notes: nil, addresses: [], phoneNumbers: [], emails: [], ristorni: [])
+    }
+}
+
+
 public struct CustomerBySP: Codable, Identifiable, Hashable, Equatable, RequestBody {
     public var id: UUID
     
