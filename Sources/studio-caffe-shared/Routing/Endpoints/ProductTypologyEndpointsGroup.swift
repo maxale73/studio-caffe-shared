c2327f76e8d9cf578bb7ed59345272d5f1dd83f1
Massimo Di Leonardo <massimodileonardo@gmail.com>
2023-11-23 07:26:23 +0100

    __

--- Sources/studio-caffe-shared/Routing/Endpoints/ProductTypologyEndpointsGroup.swift
+++ Sources/studio-caffe-shared/Routing/Endpoints/ProductTypologyEndpointsGroup.swift
@@ -27,9 +27,10 @@ public struct InventoryPurchaseDTO: Identifiable, Codable, Equatable, Hashable {
 
 public struct InventoryProductDTO: Identifiable, Codable, Equatable, Hashable {
     
-    public init(id: UUID, description: String, lastPrice: Double, iva: IvaType, purchases: [InventoryPurchaseDTO]) {
+    public init(id: UUID, description: String, packageGranularity: Double, lastPrice: Double, iva: IvaType, purchases: [InventoryPurchaseDTO]) {
         self.id = id
         self.description = description
+        self.packageGranularity = packageGranularity
         self.lastPrice = lastPrice
         self.iva = iva
         self.purchases = purchases
@@ -37,6 +38,7 @@ public struct InventoryProductDTO: Identifiable, Codable, Equatable, Hashable {
     
     public var id: UUID
     public var description: String
+    public var packageGranularity: Double
     public var lastPrice: Double
     public var iva: IvaType
     public var purchases: [InventoryPurchaseDTO]
