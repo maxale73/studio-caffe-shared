828d02d3db4d525b5df799e28ef0a93a8e5597f3
Massimo Di Leonardo <massimodileonardo@gmail.com>
2023-12-17 06:01:04 +0100

    __

--- Sources/studio-caffe-shared/Foundation extensions/Date + Extensions.swift
+++ Sources/studio-caffe-shared/Foundation extensions/Date + Extensions.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 public extension Date {
+    
     static func nowComponents(calendar: Calendar) -> DateComponents {
         calendar.dateComponents([.year, .month, .day, .minute, .second], from: Date())
     }
@@ -16,6 +17,14 @@ public extension Date {
         let beginOfLastYear = calendar.date(byAdding: .year, value: -1, to: .beginOfYear)
         let endOfLastYearDay = calendar.date(byAdding: .day, value: -1, to: .beginOfYear)
         return (beginOfLastYear!, .endOfDay(from: endOfLastYearDay!))
+    }
+    
+    static func pastYears(calendar: Calendar, offset: Int) -> (from: Date, to: Date, year: Int) {
+        let nowComponents = calendar.dateComponents([.year, .month, .day, .minute, .second], from: Date())
+        let year = nowComponents.year! - offset
+        let beginOfYear = calendar.date(from: DateComponents(calendar: calendar, year: year, month: 1, day: 1, hour: 0, minute: 1))
+        let endOfYear = calendar.date(from: DateComponents(calendar: calendar, year: year, month: 12, day: 31, hour: 11, minute: 59))
+        return (beginOfYear!, endOfYear!, year)
     }
     
     static func ultimoAnno(calendar: Calendar) -> (from: Date, to: Date) {
