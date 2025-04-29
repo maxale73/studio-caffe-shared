cef3734a14449d2d268cf6b52a7aa1b53e0a3d9a
Massimo Di Leonardo <massimodileonardo@gmail.com>
2025-04-29 04:47:32 +0200

    macos platform

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "studio-caffe-shared",
     platforms: [
-        .macOS("12.2"), .iOS("15.0")
+        .macOS("15.0"), .iOS("15.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
