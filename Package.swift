92fa1419504a054a5c0a81fe9415eb31e5ad1182
Massimo Di Leonardo <massimodileonardo@gmail.com>
2025-05-01 06:29:52 +0200

    upgrade to swift 6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "studio-caffe-shared",
     platforms: [
-        .macOS("15.0"), .iOS("15.0")
+        .macOS("15.0"), .iOS("18.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -23,7 +23,26 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "studio-caffe-shared",
-            dependencies: []),
+            dependencies: [],
+            swiftSettings: [
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_FORWARD_TRAILING_CLOSURES"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_CONCISE_MAGIC_FILE"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_IMPORT_MODULE_AS_PATH"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_DEPRECATE_APPLICATION_MAIN"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_DISABLE_OUTWARD_ACTOR_ISOLATION"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_GLOBAL_ACTOR_ISOLATED_TYPES_USABILITY"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_IMPLICIT_OPEN_EXISTENTIALS"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_IMPORT_OBJC_FORWARD_DECLS"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_INFER_SENDABLE_FROM_CAPTURES"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_ISOLATED_DEFAULT_VALUES"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_GLOBAL_CONCURRENCY"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_MEMBER_IMPORT_VISIBILITY"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_NONFROZEN_ENUM_EXHAUSTIVITY"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_REGION_BASED_ISOLATION"),
+                    .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_EXISTENTIAL_ANY"),
+                    .enableUpcomingFeature("SWIFT_STRICT_CONCURRENCY"),
+                ]),
+        
         .testTarget(
             name: "studio-caffe-sharedTests",
             dependencies: ["studio-caffe-shared"]),
