09cfbc44fb72e9e0c74a801fac2267f937cd4f22
Massimo Di Leonardo <massimodileonardo@gmail.com>
2026-03-30 04:39:18 +0200

    __

--- Sources/studio-caffe-shared/Routing/Endpoints/ChartsDataEndpointsGroup.swift
+++ Sources/studio-caffe-shared/Routing/Endpoints/ChartsDataEndpointsGroup.swift
@@ -102,6 +102,69 @@ public struct ChartYearlyTallyCashboxResponse: Identifiable, Codable, Equatable,
     public var points: [ChartYearlyTallyCashboxPoint]
 }
 
+public struct ChartYearlyRistornoVendutoRatioRequest: Identifiable, Codable, Equatable, Sendable, RequestBody {
+    public init(
+        id: UUID = .init(),
+        requestDescription: String = "",
+        startYear: Int = 2020,
+        endYear: Int? = nil
+    ) {
+        self.id = id
+        self.requestDescription = requestDescription
+        self.startYear = startYear
+        self.endYear = endYear
+    }
+
+    public var id: UUID
+    public var requestDescription: String
+    public var startYear: Int
+    public var endYear: Int?
+}
+
+public struct ChartYearlyRistornoVendutoRatioPoint: Identifiable, Codable, Equatable, Sendable {
+    public init(
+        id: UUID = .init(),
+        year: Int,
+        vendutoTotal: Double,
+        ristornoDueTotal: Double,
+        erogazioniTotal: Int,
+        customersCount: Int,
+        ratioPercent: Double
+    ) {
+        self.id = id
+        self.year = year
+        self.vendutoTotal = vendutoTotal
+        self.ristornoDueTotal = ristornoDueTotal
+        self.erogazioniTotal = erogazioniTotal
+        self.customersCount = customersCount
+        self.ratioPercent = ratioPercent
+    }
+
+    public var id: UUID
+    public var year: Int
+    public var vendutoTotal: Double
+    public var ristornoDueTotal: Double
+    public var erogazioniTotal: Int
+    public var customersCount: Int
+    public var ratioPercent: Double
+}
+
+public struct ChartYearlyRistornoVendutoRatioResponse: Identifiable, Codable, Equatable, Sendable {
+    public init(
+        id: UUID = .init(),
+        responseDescription: String,
+        points: [ChartYearlyRistornoVendutoRatioPoint]
+    ) {
+        self.id = id
+        self.responseDescription = responseDescription
+        self.points = points
+    }
+
+    public var id: UUID
+    public var responseDescription: String
+    public var points: [ChartYearlyRistornoVendutoRatioPoint]
+}
+
 public struct ChartPurchasePeriod: Identifiable, Codable, Equatable, Sendable {
     public init(id: UUID = .init(), interval: CustomTimeInterval, periodDescription: String, purchaseValue: Double, purchaseAmount: Double, sellValue: Double = 0.0, sellAmount: Int = 0) {
         self.id = id
@@ -158,6 +221,12 @@ public struct ChartDataEndpointsGroup: EndpointGroupType {
 
     public static func fetchChartYearlyTallyCashboxData(dataRequest: RequestBody? = nil) -> EndpointConfiguration {
         let parameters = [PathParameter(name: "fetch_chart_yearly_tally_cashbox_data", value: nil)]
+        let constructor = PathConstructor(group: group, elements: parameters)
+        return EndpointConfiguration(pathConstructor: constructor, method: .post, body: dataRequest)
+    }
+
+    public static func fetchChartYearlyRistornoVendutoRatioData(dataRequest: RequestBody? = nil) -> EndpointConfiguration {
+        let parameters = [PathParameter(name: "fetch_chart_yearly_ristorno_venduto_ratio_data", value: nil)]
         let constructor = PathConstructor(group: group, elements: parameters)
         return EndpointConfiguration(pathConstructor: constructor, method: .post, body: dataRequest)
     }
